76b52163f0803ada401463eda9c0e936ca2d9da1
jhoun <jayhoun@gmail.com>
2017-02-09 20:15:03 -1000

    passed 2nd by searching pokemon by id

--- routes/pokedex.js
+++ routes/pokedex.js
@@ -10,10 +10,38 @@ const elasticClient = new elasticsearch.Client({
 router.route('/')
   .get((req,res) => {
     elasticClient.search({
+      index:"pokedex",
+      type: "pokemon"
     })
       .then((body) => {
         var allPokemon = body.hits.hits.map((pokemon) => {
           return pokemon._source;
+        })
+        res.json(allPokemon);
+    })
+      .catch((e) => {
+        console.error(e);
+        res.json(card);
+    });
+  })
+
+router.route('/:id')
+  .get((req,res) => {
+    console.log('req.params.id: ', req.params.id);
+    elasticClient.search({
+      index:"pokedex",
+      body: {
+        query: {
+          match: {
+            _id: req.params.id
+          }
+        }
+      },
+    })
+      .then((body) => {
+        console.log('body: ', body);
+        var allPokemon = body.hits.hits.map((pokemon) => {
+          return pokemon._source.name;
         })
         res.json(allPokemon);
     })
--- tests/server.spec.js
+++ tests/server.spec.js
@@ -8,15 +8,24 @@ describe('GET /api/pokedex', function() {
   it('respond with redirecting to /api/pokedex', function(done) {
     request(app)
       .get('/api/pokedex')
-      .type('form')
+      .expect('Content-Type', /json/)
+      .expect(200, done)
+  })
+});
+
+describe('GET /api/pokedex/:id', function() {
+  it('respond with redirecting to /api/pokedex', function(done) {
+    request(app)
+      .get('/api/pokedex/002')
       .expect('Content-Type', /json/)
       .expect(200)
       .end(function (err, res) {
         if (err) {
           throw new Error(err);
         }
+        console.log(res);
+        expect(res.body[0]).to.equal('ivysaur')
         done()
       });
   })
-
 });
